5ed17e2c71ee91722ca844e636031c5e41280db8
spartan-alee <andy.lee@spartanbio.com>
2022-02-04 21:40:16 -0500

    Switching click handler to class method and using getter/setters

--- core.js
+++ core.js
@@ -1,15 +1,14 @@
 class Grid {
+    #focused_cell = null;
+    table_element = null;
+
     constructor(element, header_data, data) {
         this.element = element;
         this.header_data = header_data;
         this.data = data;
 
-        this._focused_call = null;
-
         this._createTable();
     }
-
-    
 
     _createTable() {
         this.element.classList.add("coresheet_container");
@@ -22,6 +21,7 @@ class Grid {
         table.setAttribute("unselectable", "yes");
         table.setAttribute("cellspacing", 0);
         table.setAttribute("cellpadding", 0);
+        this.table_element = table;
         inner_el.appendChild(table);
     
         let thead = document.createElement("thead");
@@ -82,12 +82,33 @@ class Grid {
         const all_cells = table.querySelectorAll("td");
         for (const cell of all_cells)
         {
-            cell.addEventListener("click", tableCellOnClick)
+            cell.addEventListener("click", (e) => this.tableCellOnClick(e))
             cell.setAttribute("data-logical-state", "neutral");
         }
     }
+
+    set focusedCell(cell_element) {
+        this.#focused_cell = cell_element;
+        const all_cells = this.table_element.querySelectorAll("td");
+        for (const cell of all_cells)
+        {
+            cell.setAttribute("data-logical-state", "neutral");
+        }
+        this.#focused_cell.setAttribute("data-logical-state", "focus");
+    }
+
+    get focusedCell() {
+
+    }
+
+    tableCellOnClick(e) {
+        //const parent_table = e.target.closest("table");
+        this.focusedCell = e.target;
+
+    }
 }
 
+/*
 function createTable(el, header, data) {
     el.classList.add("coresheet_container");
     let inner_el = document.createElement("div");
@@ -178,3 +199,4 @@ function tableCellOnClick(e) {
 
     }
 }
+*/
